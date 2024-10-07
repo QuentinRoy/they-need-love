db24bcd58b6a069fc8baa8bd59f2bc97c8744786
Quentin Roy <quentin@quentinroy.fr>
2024-10-07 15:15:38 +0200

    workspace

--- prisma/migrations/20241007130740_init/migration.sql
+++ prisma/migrations/20241007130740_init/migration.sql
@@ -1,7 +1,15 @@
 -- CreateTable
+CREATE TABLE "Workspace" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Workspace_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Member" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "workspaceId" INTEGER NOT NULL,
 
     CONSTRAINT "Member_pkey" PRIMARY KEY ("id")
 );
@@ -15,6 +23,7 @@ CREATE TABLE "Operation" (
     "description" TEXT,
     "date" TIMESTAMP(3) NOT NULL,
     "creditorId" INTEGER NOT NULL,
+    "workspaceId" INTEGER NOT NULL,
 
     CONSTRAINT "Operation_pkey" PRIMARY KEY ("id")
 );
@@ -53,7 +62,13 @@ CREATE UNIQUE INDEX "_OperationDebtor_AB_unique" ON "_OperationDebtor"("A", "B")
 CREATE INDEX "_OperationDebtor_B_index" ON "_OperationDebtor"("B");
 
 -- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Operation" ADD CONSTRAINT "Operation_creditorId_fkey" FOREIGN KEY ("creditorId") REFERENCES "Member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Operation" ADD CONSTRAINT "Operation_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "OperationAttachment" ADD CONSTRAINT "OperationAttachment_operationId_fkey" FOREIGN KEY ("operationId") REFERENCES "Operation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
